147102bfaef03e4c0804275e9b6a7c643d495a82
Inplex-sys <Inplex-sys+VBTuMirEZrscYlFx@protonmail.ch>
2024-06-08 21:58:16 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,56 +1,74 @@
 #!/bin/bash
 
 help() {
-    echo "Usage: $0 <arch> [--list]"
-    echo ""
-    echo "Supported architectures:" 
-    echo "  • armv7l - Mostly used in IoT devices"
-    echo "  • aarch64 - Most used in SBCs and phones"
-    echo "  • i386 - Most common 32-bit architecture"
-    echo "  • x86_64 - Most common 64-bit architecture"
-    echo "  • ppc64le - Used in IBM Power Systems"
-    echo "  • mips64 - Used in routers and IoT devices"
-    exit 0
+        echo "Usage: $0 <arch> <entrypoint> [--list] [--imports import1,import2,...]"
+        echo ""
+        echo "Supported architectures:" 
+        echo "  • armv7l - Mostly used in IoT devices"
+        echo "  • aarch64 - Most used in SBCs and phones"
+        echo "  • i386 - Most common 32-bit architecture"
+        echo "  • x86_64 - Most common 64-bit architecture"
+        echo "  • ppc64le - Used in IBM Power Systems"
+        echo "  • mips64 - Used in routers and IoT devices"
+        exit 0
 }
 
-if [ "$#" -ne 1 ]; then
-    help
-    exit 1
+if [ "$#" -lt 2 ]; then
+        help
+        exit 1
 fi
 
 ARCH=$1
+ENTRYPOINT=$2
+IMPORTS=""
+
+# Parse additional arguments
+shift 2 # Skip first two arguments
+while (( "$#" )); do
+    case "$1" in
+        --imports)
+            IFS=',' read -ra ADDR <<< "$2"
+            for i in "${ADDR[@]}"; do
+                IMPORTS+=" --hidden-import $i"
+            done
+            shift 2
+            ;;
+        --list)
+            help
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
 
 run_docker() {
-    echo -e "\e[34mBuilding for $ARCH...\e[0m\c"
+        echo -e "\e[34mBuilding for $ARCH with entrypoint $ENTRYPOINT and imports $IMPORTS...\e[0m\c"
 
-    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes > /dev/null 2>&1
+        docker run --rm --privileged multiarch/qemu-user-static --reset -p yes > /dev/null 2>&1
 
-    docker run --platform $1 --rm -t -v $(pwd):/root/ $2 /bin/bash -c "apt -qq update; \
-    apt -qq install gcc zlib1g-dev -y; \
-    pip3 -q install --upgrade pip; \
-    pip3 install psutil colored py-cpuinfo requests pyinstaller; \
-    pyinstaller /root/src/main.py --distpath /root/ --onefile --clean --hidden-import py-cpuinfo --hidden-import colored --hidden-import psutil --hidden-import requests > /dev/null; \
-    mv /root/main /root/main-$ARCH"
+        docker run --platform $1 --rm -t -v $(pwd):/root/ $2 /bin/bash -c "apt -qq update; \
+        apt -qq install gcc zlib1g-dev -y; \
+        pip3 -q install --upgrade pip; \
+        pip3 install psutil colored py-cpuinfo requests pyinstaller; \
+        pyinstaller /root/src/$ENTRYPOINT --distpath /root/ --onefile --clean $IMPORTS > /dev/null; \
+        mv /root/$ENTRYPOINT /root/$ENTRYPOINT-$ARCH"
 
-    if [ $? -ne 0 ]; then
-        echo -e "[\e[31mERROR\e[0m] Build failed"
-        exit 1
-    fi
+        if [ $? -ne 0 ]; then
+                echo -e "[\e[31mERROR\e[0m] Build failed"
+                exit 1
+        fi
 
-    echo -e "[\e[32mOK\e[0m] Build completed successfully"
-    rm -rf ./.cache
+        echo -e "[\e[32mOK\e[0m] Build completed successfully"
+        rm -rf ./.cache
 }
 
-if [ "$1" = "--list" ]; then
-    help
-fi
-
 case $ARCH in
-    "x86_64") run_docker "linux/amd64" "amd64/python:3.6-jessie" ;;
-    "i386") run_docker "linux/i386" "i386/python:3.6-jessie" ;;
-    "armv7l") run_docker "linux/arm" "arm32v7/python:3.6-jessie" ;;
-    "aarch64") run_docker "linux/arm64" "arm64v8/python:3.6-jessie" ;;
-    "ppc64le") run_docker "linux/ppc64le" "ppc64le/python:3.6-jessie" ;;
-    "mips64") run_docker "linux/mips64le" "mips64le/python:3.9.0a5-buster" ;;
-    *) help;;
+        "x86_64") run_docker "linux/amd64" "amd64/python:3.6-jessie" ;;
+        "i386") run_docker "linux/i386" "i386/python:3.6-jessie" ;;
+        "armv7l") run_docker "linux/arm" "arm32v7/python:3.6-jessie" ;;
+        "aarch64") run_docker "linux/arm64" "arm64v8/python:3.6-jessie" ;;
+        "ppc64le") run_docker "linux/ppc64le" "ppc64le/python:3.6-jessie" ;;
+        "mips64") run_docker "linux/mips64le" "mips64le/python:3.9.0a5-buster" ;;
+        *) help;;
 esac
